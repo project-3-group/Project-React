456b55a2da7b43d8e997d508048bec24173940c7
saifobe <obeidatsaif93@gmail.com>
2023-03-25 00:32:48 +0000

    Navbar and routs has been added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Game from './components/Game/Game';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/game' element={<Game />}></Route>
+      <Route path='/about' element={<About />} ></Route>
+    </Routes>
+    </>
   );
 }
 
