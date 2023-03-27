de3a5dd54f2c4e94180611451ead198ce3aed6e6
asfantala <22037049@student.ltuc.com>
2023-03-27 19:09:44 +0000

    styling branch

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap';
-// import logo from './logo.svg';
 
 function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand href="/">
-          {/* <img
-            // src={logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt="Logo"
-          /> */}
+
           GeoGenius
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
