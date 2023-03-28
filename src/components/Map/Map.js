8e47cddd24d5f0fbfb50307239a6ce4f56a6c3fd
Emam Mounqer <emam.anagreh@gmail.com>
2023-03-28 13:32:53 +0000

    remove sphere from the map and add on hover btn

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,17 +1,56 @@
 import "react-tooltip/dist/react-tooltip.css";
 import { Tooltip } from "react-tooltip";
 import MapChart from "./MapChart";
+import { useEffect, useState } from "react";
 
-function Map({ onSelectCountry, highlightSelected,showNameOnHover = true }) {
+
+function Map({
+  onSelectCountry,
+  highlightSelected,
+  showNameOnHoverDef = true,
+}) {
+  const [showNameOnHover, setShowNameOnHover] = useState(showNameOnHoverDef);
+
+  const toggleShowNameOnHover = () => setShowNameOnHover(!showNameOnHover);
   return (
     <div>
       <MapChart
         onSelectCountry={onSelectCountry}
         highlightSelected={highlightSelected}
+        toggleShowNameOnHover={toggleShowNameOnHover}
+        showNameOnHover={showNameOnHover}
       />
-      {showNameOnHover && <Tooltip anchorSelect=".country-element" />}
-      
+      {showNameOnHover && <ToolTipOnMouse/>}
     </div>
   );
 }
+
+function ToolTipOnMouse() {
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+
+  const handleMouseMove = event => {
+    setMousePosition({
+      x: event.clientX,
+      y: event.clientY
+    });
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousemove' , handleMouseMove)
+  
+    return () => {
+      document.removeEventListener('mousemove' , handleMouseMove)
+
+    }
+  }, [])
+  
+
+  return (
+    <Tooltip
+      anchorSelect=".country-element"
+      position={mousePosition}
+    />
+  );
+}
+
 export default Map;
