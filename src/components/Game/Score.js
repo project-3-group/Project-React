f63f3564e9b69cffb70f32daf8f88bcc33f86d47
dinajar <deenajaradat2@gmail.com>
2023-03-30 02:14:18 +0300

    Game_Page_style

--- src/components/Game/Score.js
+++ src/components/Game/Score.js
@@ -7,14 +7,14 @@ function Score(props) {
 
     
     return (
-        <div style={{ width: '10rem', height: '4rem', marginRight: '20rem' }}>
-            <Card style={{ marginBottom: '0.5rem' , backgroundColor:'#272733', color: 'white'}}>
+        <div style={{ width: '10rem', height: '4rem', marginRight: '20rem', marginBottom:'5rem' , marginTop:'5rem'}}>
+            <Card style={{ marginBottom: '0.5rem' , backgroundColor:'#0a2647', color: 'white'}}>
                 <Card.Body style={{ textAlign: 'center', fontWeight: 'bold' }}>
-                    {`SCORE: ${props.score.toString()}`}
+                    <i class="fa-solid fa-star" style={{ color:'#FFB100' }}>  {`${props.score.toString()}`}  </i>
                     {console.log(props.score)}
                 </Card.Body>
             </Card>
-            <Card style={{ marginBottom: '0.5rem' , backgroundColor:'#272733', color: 'white'}}>
+            <Card style={{ marginBottom: '0.5rem' , backgroundColor:'#0a2647', color: 'white'}}>
                 <Card.Body style={{ textAlign: 'center', fontWeight: 'bold' }}>
                     {`YOUR HIGHEST: ${props.highestScore}`}
                 </Card.Body>
